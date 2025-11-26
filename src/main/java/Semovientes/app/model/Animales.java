680ee171a4c8896822249dfa19c50dca567265d0
odlanod <djgarcia548@gmail.com>
2025-11-26 11:41:34 -0500

    Update Animales.java
    Se definió la clase Animales.java

--- src/main/java/Semovientes/app/model/Animales.java
+++ src/main/java/Semovientes/app/model/Animales.java
@@ -6,104 +6,44 @@ import jakarta.persistence.*;
 public class Animales {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-
     private int id_animal;
+    
+    @ManyToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name="finca_id")
     private int id_finca;
-    private String tipo_animal;
+
+    @Column(name="nombre_animal", nullable=true, length=40)
+    private String nombre;
+
+    @Column(name="especie_animal", nullable = false, length=30)
+    private String especie;
+
+    @Column(name="raza_animal", nullable=false, length=30)
     private String raza;
-    private String fecha_nacimiento;
+
+    @Column=(name="sexo_animal", nullable=false, length=20)
     private String sexo;
-    private double peso;
-    private String estado_salud;
-    private String informacion_adicional;
+
+    @Column(name="tipo_adquisicion", nullable= false, length=30)
+    private String adquisicion;
+
+    @Column(name="fecha", nullable= false)
+    private LocalDate fecha;
+
+    @Column(name="peso_inicial", nullable= false, length= 20)
+    private double pesoInicial;
+
+    @Column(name="estado_animal", nullable=false, length=30)
+    private String estadoAnimal;
+
+    @ManyToOne
+    @JoinColumn(name="id_madre",nullable=false)
+    private Animales id_madre;
+
+    @ManyToOne
+    @JoinColumn(name="id_padre",nullable=false)
+    private Animales id_padre;
 
 
-    public Animales(int id_animal, int id_finca, String tipo_animal, String raza, String fecha_nacimiento, String sexo, double peso, String estado_salud, String informacion_adicional) {
-        this.id_animal = id_animal;
-        this.id_finca = id_finca;
-        this.tipo_animal = tipo_animal;
-        this.raza = raza;
-        this.fecha_nacimiento = fecha_nacimiento;
-        this.sexo = sexo;
-        this.peso = peso;
-        this.estado_salud = estado_salud;
-        this.informacion_adicional = informacion_adicional;
-    }
-    public Animales(){   }
-
-    //GETTER AND SETTER---------------------------------------------
-
-
-    public int getId_animal() {
-        return id_animal;
-    }
-
-    public void setId_animal(int id_animal) {
-        this.id_animal = id_animal;
-    }
-
-    public int getId_finca() {
-        return id_finca;
-    }
-
-    public void setId_finca(int id_finca) {
-        this.id_finca = id_finca;
-    }
-
-    public String getTipo_animal() {
-        return tipo_animal;
-    }
-
-    public void setTipo_animal(String tipo_animal) {
-        this.tipo_animal = tipo_animal;
-    }
-
-    public String getRaza() {
-        return raza;
-    }
-
-    public void setRaza(String raza) {
-        this.raza = raza;
-    }
-
-    public String getFecha_nacimiento() {
-        return fecha_nacimiento;
-    }
-
-    public void setFecha_nacimiento(String fecha_nacimiento) {
-        this.fecha_nacimiento = fecha_nacimiento;
-    }
-
-    public String getSexo() {
-        return sexo;
-    }
-
-    public void setSexo(String sexo) {
-        this.sexo = sexo;
-    }
-
-    public double getPeso() {
-        return peso;
-    }
-
-    public void setPeso(double peso) {
-        this.peso = peso;
-    }
-
-    public String getEstado_salud() {
-        return estado_salud;
-    }
-
-    public void setEstado_salud(String estado_salud) {
-        this.estado_salud = estado_salud;
-    }
-
-    public String getInformacion_adicional() {
-        return informacion_adicional;
-    }
-
-    public void setInformacion_adicional(String informacion_adicional) {
-        this.informacion_adicional = informacion_adicional;
-    }
 }
 */
