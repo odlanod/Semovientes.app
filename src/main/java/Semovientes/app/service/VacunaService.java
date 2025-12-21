ac960a69ea3aeb6c8ee7cb22ae72583ae555444a
odlanod <djgarcia548@gmail.com>
2025-12-21 09:59:17 -0500

    Implement VacunaService for vaccine management
    Se adiciono VacunaService al proyecto, el metodo actualizarVacuna esta incompleto

---
+++ src/main/java/Semovientes/app/service/VacunaService.java
@@ -0,0 +1,35 @@
+package Semovientes.app.service;
+import semovientes.app.model.Vacunas;
+import Semovientes.app.repository.VacunasRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+import java.util.Optional;
+
+@Service
+public class VacunaService{
+  
+private final VacunasRepository vacunaRepository;
+  public VacunaService(VacunasRepository vacunaRepository){
+    this.vacunaRepository= vacunaRepository;}
+  
+  public List<Vacunas> obtenerTodasLasVacunas(){
+    return vacunaRepository.findAll();}
+
+  public Optional<Vacunas> obtenerVacunasPorId(int id){
+    return vacunaRepository.findById(id);}
+
+  public boolean eliminarVacuna(int id){
+    if(vacunaRepository.existsById(id)){
+      vacunaRepository.deleteById(id);
+      return true;
+    }else{return false;}}
+
+  public Vacunas guardarVacuna(Vacunas vacuna){
+    return vacunaRepository.save(vacuna);}
+
+  public Optional<Vacunas> actualizarVacuna(int id, Vacunas vacunas){
+    return vacunaRepository.findById(id)
+      .map();
+  }
+}
