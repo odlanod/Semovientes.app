a0c2243cad3c9614ccce158c6850ffaf14159536
odlanod <djgarcia548@gmail.com>
2025-12-22 10:09:43 -0500

    Rename fields in RegistroAnimales class
    Corrección de malas practicas de programación

--- src/main/java/Semovientes/app/model/RegistroAnimales.java
+++ src/main/java/Semovientes/app/model/RegistroAnimales.java
@@ -5,16 +5,16 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @Entity
-@Table(name="Registro_Animales")
+@Table(name="RegistroAnimales")
 
-public class Registro_Animales{
+public class RegistroAnimales{
 
 @Id
 @GeneratedValue(strategy = GenerationType.IDENTITY)
 private Long id;
 
-@Column(name="fecha_Registro", nullable=false)
-private LocalDate fecha_Registro;
+@Column(name="fechaRegistro", nullable=false)
+private LocalDate fechaDeRegistro;
 
 @ManyToOne(fetch= FetchType.LAZY)
 @JoinColumn(name="idUsuario", nullable=false)
@@ -24,19 +24,19 @@ private Usuario usuario;
 @JoinColumn(name="id_animal", nullable=false)
 private Animales animal;
 
-@Column(name="Tipo_Movimiento", nullable=false, length=30)
+@Column(name="TipoDeMovimiento", nullable=false, length=30)
 private String movimiento;
 
 @Column(name="Cantidad",nullable=false)
 private double cantidad;
 
-@Column(name="U/M", nullable=false, length=20)
+@Column(name="UnidadMedida", nullable=false, length=20)
 private String UM;
 
-@Column(name = "Valor_Kilogramo", precision = 10, scale = 2, nullable = false)
+@Column(name = "ValorPorKilogramo", precision = 10, scale = 2, nullable = false)
 private java.math.BigDecimal valorKilogramo; // Usa BigDecimal para precisión monetaria
 
-@Column(name = "Valor_Total", precision = 10, scale = 2, nullable = false)
+@Column(name = "ValorTotal", precision = 10, scale = 2, nullable = false)
 private java.math.BigDecimal valorTotal; // Usa BigDecimal para precisión monetaria
 
 @Column(name="Detalles", nullable=true, length=100)
@@ -45,8 +45,8 @@ private String detalles;
 
     public Registro_Animales(){}
 
-    public Registro_Animales(LocalDate fecha_Registro, String movimiento, double cantidad, String UM, BigDecimal valorKilogramo, BigDecimal valorTotal, String detalles) {
-        this.fecha_Registro = fecha_Registro;
+    public Registro_Animales(LocalDate fechaDeRegistro, String movimiento, double cantidad, String UM, BigDecimal valorKilogramo, BigDecimal valorTotal, String detalles) {
+        this.fechaDeRegistro = fechaDeRegistro;
         this.movimiento = movimiento;
         this.cantidad = cantidad;
         this.UM = UM;
@@ -59,8 +59,8 @@ private String detalles;
     public Long getId() {return id;}
     public void setId(Long id) {this.id = id;}
 
-    public LocalDate getFecha_Registro() {return fecha_Registro;}
-    public void setFecha_Registro(LocalDate fecha_Registro) {this.fecha_Registro = fecha_Registro;}
+    public LocalDate getFechaDeRegistro() {return fechaDeRegistro;}
+    public void setFechaDeRegistro(LocalDate fechaDeRegistro) {this.fechaDeRegistro = fechaDeRegistro;}
 
     public Usuario getUsuario() {return usuario;}
     public void setUsuario(Usuario usuario) {this.usuario = usuario;}
