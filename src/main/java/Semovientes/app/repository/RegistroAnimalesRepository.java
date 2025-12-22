eca10839d03ea764912257ab9191089d610795c9
odlanod <djgarcia548@gmail.com>
2025-12-22 10:43:19 -0500

    Rename Registro_Animales to RegistroAnimales in repository
    Actualización por malas practica de programación

--- src/main/java/Semovientes/app/repository/RegistroAnimalesRepository.java
+++ src/main/java/Semovientes/app/repository/RegistroAnimalesRepository.java
@@ -1,8 +1,8 @@
 package Semovientes.app.repository;
-import Semovientes.app.model.Registro_Animales;
+import Semovientes.app.model.RegistroAnimales;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RegistroAnimalesRepository extends JpaRepository<Registro_Animales,Long> {
+public interface RegistroAnimalesRepository extends JpaRepository<RegistroAnimales,Long> {
 }
